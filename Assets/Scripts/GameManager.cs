1ae22c9e3e97864d8e88bb1eb99b72daa9fb99cd
Daniëlle <moontowhee@gmail.com>
2020-12-03 12:55:26 +0100

    First Update Reupload

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;
+
+    public GameObject youWinText;
+    public float resetDelay;
+
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != null) //! does not...
+            Destroy(gameObject); //no 2 game managers
+    }
+
+    public void Win()
+    {
+        //The winning message
+        youWinText.SetActive (true);
+        Time.timeScale = .5f; //Dramatic effect when winning the game
+        //Invoke("Reset", resetDelay); //It resets the game, an not needed but handy.
+    }
+
+    void Reset()
+    {
+        Time.timeScale = 1.0f;
+        Application.LoadLevel(Application.loadedLevel);
+    }
+}
---
+++ Assets/Scripts/quit.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class quit : MonoBehaviour
+{
+    public Texture2D cursorArrow;
+
+    void OnGUI() //Mouse appears on UI: Quit canvas.
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    public void doquit()
+    {
+        Debug.Log("has quit game");
+        Application.Quit();
+    }
+
+    void Start()
+    {
+        Cursor.SetCursor(cursorArrow, Vector2.zero, CursorMode.ForceSoftware);
+    }
+
+}
